942ab29068f220ab7e9936c860b486f5b39b954c
Freddy Esteban Perez <fredstban@icloud.com>
2023-08-08 10:03:14 -0500

    update dev port

--- components/navigation-events.js
+++ components/navigation-events.js
@@ -6,7 +6,7 @@ import { usePathname } from 'next/navigation'
 export default function NavigationEvents() {
   const pathname = usePathname()
   useEffect(() => {
-    fetch('http://localhost:3000/visits/api', {
+    fetch('http://localhost:3001/visits/api', {
       method: 'POST',
       body: JSON.stringify({ page: pathname })
     })
