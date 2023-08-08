9af2aa92c5824d652cdeabad364702739d2f48f0
Freddy Esteban Perez <fredstban@icloud.com>
2023-08-08 06:14:46 -0500

    Add bootstrap

---
+++ app/visits/page.js
@@ -0,0 +1,15 @@
+import { Container } from 'react-bootstrap'
+
+export default function Visits() {
+  return (
+    <main>
+      <div className='container'>
+        <div className='row'>
+          <div className='d-flex justify-content-center py-2'>
+            <h2>Site Visits</h2>
+          </div>
+        </div>
+      </div>
+    </main>
+  )
+}
